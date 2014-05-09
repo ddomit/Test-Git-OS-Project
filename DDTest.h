6c741b4eb0f8a4546fd64c857fd4d1ce2d7679ee
ddomit <pothial@gmail.com>
2014-05-08 21:03:30 -0500

    added test class

---
+++ DDTest.h
@@ -0,0 +1,13 @@
+//
+//  DDTest.h
+//  GIT Test
+//
+//  Created by Daniel Domit on 5/8/14.
+//  Copyright (c) 2014 Daniel Domit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDTest : NSObject
+
+@end
---
+++ DDTest.m
@@ -0,0 +1,13 @@
+//
+//  DDTest.m
+//  GIT Test
+//
+//  Created by Daniel Domit on 5/8/14.
+//  Copyright (c) 2014 Daniel Domit. All rights reserved.
+//
+
+#import "DDTest.h"
+
+@implementation DDTest
+
+@end
